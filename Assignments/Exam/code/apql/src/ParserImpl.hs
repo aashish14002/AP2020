a11c2f3bdfc02fbec8f81eb713c90c32e7f4da26
Aashish Kumar <aashish14002@iiitd.ac.in>
2020-11-01 23:42:28 +0100

    change tokenSpace Parser name and fix identation

--- Assignments/Exam/code/apql/src/ParserImpl.hs
+++ Assignments/Exam/code/apql/src/ParserImpl.hs
@@ -11,235 +11,235 @@ type Parser a = ReadP a
 
 resKeywords = ["and", "false", "if", "implies", "in", "is", "not", "or", "true", "unless"]
 
-tokenSpaceAndComment :: Parser a -> Parser a
-tokenSpaceAndComment a = do
-                            skipSpaces
-                            p <- a
-                            skipSpaces
-                            return p
+tokenSpace :: Parser a -> Parser a
+tokenSpace a = do
+                    skipSpaces
+                    p <- a
+                    skipSpaces
+                    return p
 
 parseString :: String -> Either ErrMsg Program
 parseString pgrm = case readP_to_S program pgrm of
-                    [] -> Left (EUser "no Parse")
-                    x -> case last x of
-                              (e, "") -> Right e
-                              _ -> Left (EInternal "no Parse")
+            [] -> Left (EUser "no Parse")
+            x -> case last x of
+                        (e, "") -> Right e
+                        _ -> Left (EInternal "no Parse")
 
 program :: Parser Program
-program = tokenSpaceAndComment (do
-                                    r <- rule
-                                    skipComments
-                                    char '.'
-                                    skipComments
-                                    p <- program
-                                    return (r:p)
-                                <|> return [])
+program = tokenSpace (do
+                        r <- rule
+                        skipComments
+                        char '.'
+                        skipComments
+                        p <- program
+                        return (r:p)
+                    <|> return [])
 
 rule :: Parser Rule
-rule = tokenSpaceAndComment (do
-                                a <- atom
-                                rule' a)
+rule = tokenSpace (do
+                        a <- atom
+                        rule' a)
 
 rule' :: Atom -> Parser Rule
-rule' atm = tokenSpaceAndComment (do 
-                                        skipComments
-                                        string "if"
-                                        skipComments
-                                        c <- cond
-                                        return (Rule atm c)
-                                    <|>
-                                    do 
-                                        skipComments
-                                        string "unless"
-                                        skipComments
-                                        c <- cond
-                                        return (Rule atm (CNot c))
-                                    <|> return (Rule atm CTrue))
+rule' atm = tokenSpace (do 
+                            skipComments
+                            string "if"
+                            skipComments
+                            c <- cond
+                            return (Rule atm c)
+                        <|>
+                        do 
+                            skipComments
+                            string "unless"
+                            skipComments
+                            c <- cond
+                            return (Rule atm (CNot c))
+                        <|> return (Rule atm CTrue))
 
 cond :: Parser Cond
-cond = tokenSpaceAndComment (do
-                                f <- condOr
-                                condImplies f)
+cond = tokenSpace (do
+                        f <- condOr
+                        condImplies f)
 
 condImplies :: Cond -> Parser Cond
-condImplies cnd = tokenSpaceAndComment (do
-                                            skipComments
-                                            string "implies"
-                                            skipComments
-                                            c <- cond
-                                            return (COr (CNot cnd) c)
-                                        <|> return cnd)
+condImplies cnd = tokenSpace (do
+                                skipComments
+                                string "implies"
+                                skipComments
+                                c <- cond
+                                return (COr (CNot cnd) c)
+                            <|> return cnd)
 
 condOr :: Parser Cond
-condOr = tokenSpaceAndComment (do
-                                    f <- condAnd
-                                    condOr' f)
+condOr = tokenSpace (do
+                        f <- condAnd
+                        condOr' f)
 
 condOr' :: Cond -> Parser Cond
-condOr' cnd = tokenSpaceAndComment (do
-                                        skipComments
-                                        string "or"
-                                        skipComments
-                                        f <- condAnd
-                                        f' <- condOr' f
-                                        return (COr cnd f')
-                                    <|> return cnd)
+condOr' cnd = tokenSpace (do
+                            skipComments
+                            string "or"
+                            skipComments
+                            f <- condAnd
+                            f' <- condOr' f
+                            return (COr cnd f')
+                        <|> return cnd)
 
 condAnd :: Parser Cond
-condAnd = tokenSpaceAndComment (do
-                                    f <- condNot
-                                    condAnd' f)
+condAnd = tokenSpace (do
+                        f <- condNot
+                        condAnd' f)
 
 condAnd' :: Cond -> Parser Cond
-condAnd' cnd = tokenSpaceAndComment (do
-                                        skipComments
-                                        string "and"
-                                        skipComments
-                                        f <- condNot
-                                        f' <- condAnd' f
-                                        return (CAnd cnd f')
-                                    <|> return cnd)
+condAnd' cnd = tokenSpace (do
+                                skipComments
+                                string "and"
+                                skipComments
+                                f <- condNot
+                                f' <- condAnd' f
+                                return (CAnd cnd f')
+                            <|> return cnd)
 
 condNot :: Parser Cond
-condNot = tokenSpaceAndComment (do
-                                    condParen
-                                <|> 
-                                do
-                                    skipComments
-                                    string "not"
-                                    skipComments
-                                    f <- condNot
-                                    return (CNot f))
+condNot = tokenSpace (do
+                        condParen
+                    <|> 
+                    do
+                        skipComments
+                        string "not"
+                        skipComments
+                        f <- condNot
+                        return (CNot f))
 
 condParen :: Parser Cond
-condParen = tokenSpaceAndComment (do
-                                    condAtom
-                                <|> 
-                                do
-                                    skipComments
-                                    char '('
-                                    skipComments
-                                    cnd <- cond
-                                    skipComments
-                                    char ')'
-                                    skipComments
-                                    return cnd)
+condParen = tokenSpace (do
+                            condAtom
+                        <|> 
+                        do
+                            skipComments
+                            char '('
+                            skipComments
+                            cnd <- cond
+                            skipComments
+                            char ')'
+                            skipComments
+                            return cnd)
 
 condAtom :: Parser Cond
-condAtom = tokenSpaceAndComment (do
-                                    atm <- atom
-                                    return (CAtom atm)
-                                <|>
-                                do
-                                    condTerm
-                                <|>
-                                do
-                                    skipComments
-                                    string "true"
-                                    skipComments
-                                    return CTrue
-                                <|>
-                                do
-                                    skipComments
-                                    string "false"
-                                    skipComments
-                                    return (CNot CTrue))
+condAtom = tokenSpace (do
+                            atm <- atom
+                            return (CAtom atm)
+                        <|>
+                        do
+                            condTerm
+                        <|>
+                        do
+                            skipComments
+                            string "true"
+                            skipComments
+                            return CTrue
+                        <|>
+                        do
+                            skipComments
+                            string "false"
+                            skipComments
+                            return (CNot CTrue))
 
 condTerm :: Parser Cond
-condTerm = tokenSpaceAndComment (do
-                                    t <- term 
-                                    skipComments
-                                    string "is"
-                                    skipComments
-                                    condTerm' t)
+condTerm = tokenSpace (do
+                            t <- term 
+                            skipComments
+                            string "is"
+                            skipComments
+                            condTerm' t)
 
 condTerm' :: Term -> Parser Cond
-condTerm' trm = tokenSpaceAndComment (do
-                                        t <- term
-                                        return (CEq trm t)
-                                    <|>
-                                    do
-                                        skipComments
-                                        string "not"
-                                        skipComments
-                                        t <- term
-                                        return (CNot (CEq trm t)))
+condTerm' trm = tokenSpace (do
+                                t <- term
+                                return (CEq trm t)
+                            <|>
+                            do
+                                skipComments
+                                string "not"
+                                skipComments
+                                t <- term
+                                return (CNot (CEq trm t)))
 
 atom :: Parser Atom
-atom = tokenSpaceAndComment (do
-                                p <- pName
-                                skipComments
-                                char '('
-                                skipComments
-                                tz <- termz
-                                skipComments
-                                char ')'
-                                skipComments
-                                return (Atom p tz))
+atom = tokenSpace (do
+                        p <- pName
+                        skipComments
+                        char '('
+                        skipComments
+                        tz <- termz
+                        skipComments
+                        char ')'
+                        skipComments
+                        return (Atom p tz))
 
 termz :: Parser [Term]
-termz = tokenSpaceAndComment (do
-                                terms
-                            <|> return [])
+termz = tokenSpace (do
+                        terms
+                    <|> return [])
 
 terms :: Parser [Term]
-terms = tokenSpaceAndComment (do
-                                t <- term
-                                terms' t)
+terms = tokenSpace (do
+                        t <- term
+                        terms' t)
 
 terms' :: Term -> Parser [Term]
-terms' trm = tokenSpaceAndComment (do
-                                    skipComments
-                                    char ','
-                                    skipComments
-                                    t <- terms
-                                    return (trm:t)
-                                <|>
-                                    return [trm])
+terms' trm = tokenSpace (do
+                            skipComments
+                            char ','
+                            skipComments
+                            t <- terms
+                            return (trm:t)
+                        <|>
+                            return [trm])
 
 term :: Parser Term
-term = tokenSpaceAndComment (do
-                                v <- vName
-                                return (TVar v)
-                            <|>
-                            do
-                                d <- constData
-                                return (TData d))
+term = tokenSpace (do
+                        v <- vName
+                        return (TVar v)
+                    <|>
+                    do
+                        d <- constData
+                        return (TData d))
 
 pName :: Parser PName
-pName = tokenSpaceAndComment name
+pName = tokenSpace name
 
 vName :: Parser VName
-vName = tokenSpaceAndComment name
+vName = tokenSpace name
 
 constData :: Parser Data
-constData = tokenSpaceAndComment (do
-                                    skipComments
-                                    char '\"'
-                                    c <- pData
-                                    char '\"'
-                                    skipComments
-                                    return c)
+constData = tokenSpace (do
+                            skipComments
+                            char '\"'
+                            c <- pData
+                            char '\"'
+                            skipComments
+                            return c)
 
 pData :: Parser String
-pData = tokenSpaceAndComment (do
-                                d <- doubleQuotes
-                                t <- pData
-                                return (d ++ t)
-                            <|>
-                            do
-                                d <- satisfy (\c -> isPrint c && c /= '\"')
-                                t <- pData
-                                return (d:t )
-                            <|>
-                                return "")
+pData = tokenSpace (do
+                        d <- doubleQuotes
+                        t <- pData
+                        return (d ++ t)
+                    <|>
+                    do
+                        d <- satisfy (\c -> isPrint c && c /= '\"')
+                        t <- pData
+                        return (d:t )
+                    <|>
+                        return "")
 
 
 doubleQuotes :: Parser String
-doubleQuotes = tokenSpaceAndComment (do
-                                        char '\"'
-                                        char '\"'
-                                        return "\"")
+doubleQuotes = tokenSpace (do
+                                char '\"'
+                                char '\"'
+                                return "\"")
 
 name :: Parser String
 name = do 
@@ -253,10 +253,10 @@ name = do
 
 skipComments :: Parser ()
 skipComments = do
-                    skipSpaces
-                    string "(*"
-                    manyTill get (string "*)")
-                    skipSpaces
-                    skipComments
-                <|>
-                    return ()
+            skipSpaces
+            string "(*"
+            manyTill get (string "*)")
+            skipSpaces
+            skipComments
+        <|>
+            return ()
