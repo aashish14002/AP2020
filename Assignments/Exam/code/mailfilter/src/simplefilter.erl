4d07c013e82582b82232e3698f0662911091191d
Aashish Kumar <aashish14002@iiitd.ac.in>
2020-11-02 04:21:21 +0100

    [WIP] update simple filter

--- Assignments/Exam/code/mailfilter/src/simplefilter.erl
+++ Assignments/Exam/code/mailfilter/src/simplefilter.erl
@@ -9,7 +9,7 @@ importance(M, C) ->
 importance2(M, C) ->
   {transformed, <<"FOrget it">>}.
 
-oneway(Mail) ->
+oneway1(Mail) ->
   {ok, MS} = mailfilter:start(infinite),
   mailfilter:default(MS, importance, {simple, fun importance/2}, #{}),
   mailfilter:default(MS, importance1, {simple, fun importance/2}, #{}),
@@ -22,7 +22,7 @@ oneway(Mail) ->
                    % but we'll give mailfilter a fighting chance to run the filters
   mailfilter:get_config(MR).
 
-another(Mail) ->
+another1(Mail) ->
   {ok, MS} = mailfilter:start(infinite),
 
   {ok, MR} = mailfilter:add_mail(MS, Mail),
@@ -35,16 +35,37 @@ another(Mail) ->
   Res = [ Result || {Label, Result} <- Config],
   {M, lists:nth(1, Res)}.
 
-try_it() ->
-  Mail = <<"Remember to read AP exam text carefully">>,
-  {MS, Res1} = oneway(Mail),
-  {_M, Res2} = another(Mail),
-  {ok, _} = mailfilter:stop(MS),
-  Translate = fun (Result) ->
-                  case Result of
-                    {done, #{spam := true}} -> "We git spam - nom nom nom";
-                    {done, #{importance := N}} when N > 69 -> "IMPORTANT!";
-                    {done, _} ->                   "Can wait for tomorrow";
-                    inprogress ->                   "Need a new computer!"
-                  end end,
-  io:fwrite("One way: ~s~nor another: ~s~n" , [Translate(Res1), Translate(Res2)]).
+  oneway(Mail) ->
+    {ok, MS} = mailfilter:start(infinite),
+    mailfilter:default(MS, importance, {simple, fun importance/2}, #{}),
+    {ok, MR} = mailfilter:add_mail(MS, Mail),
+    timer:sleep(50), % Might not be needed,
+                     % but we'll give mailfilter a fighting chance to run the filters
+    {ok, [{importance, Res}]} = mailfilter:get_config(MR),
+    {MS, Res}.
+  
+  another(Mail) ->
+    {ok, MS} = mailfilter:start(infinite),
+    {ok, MR} = mailfilter:add_mail(MS, Mail),
+    mailfilter:add_filter(MR, "importance", {simple, fun importance/2}, #{}),
+    mailfilter:add_filter(MR, ap_r0cks, {simple, fun(<<M:24/binary, _>>) ->
+                                                     {transform, M} end}, flap),
+    timer:sleep(50), % Might not be needed
+    {ok, [{M, Config}]} = mailfilter:stop(MS),
+    [Res] = [ Result || {Label, Result} <- Config, Label == "importance"],
+    {M, Res}.
+  
+  try_it() ->
+    Mail = <<"Remember to read AP exam text carefully">>,
+    {MS, Res1} = oneway(Mail),
+    {_M, Res2} = another(Mail),
+    {ok, _} = mailfilter:stop(MS),
+    Translate = fun (Result) ->
+                    case Result of
+                      {done, #{spam := true}} -> "We git spam - nom nom nom";
+                      {done, #{importance := N}} when N > 69 -> "IMPORTANT!";
+                      {done, _} ->                   "Can wait for tomorrow";
+                      inprogress ->                   "Need a new computer!"
+                    end end,
+    io:fwrite("One way: ~s~nor another: ~s~n" , [Translate(Res1), Translate(Res2)]).
+  
